372a2f37d08ec29394dff4692f81480b0ce568fc
M U N I K U M A R <73020070+munikumar2527@users.noreply.github.com>
2021-04-22 14:44:40 +0530

    Add files via upload

---
+++ test/src/main/java/com/test/StudentEntity.java
@@ -0,0 +1,29 @@
+package com.test;
+
+public class StudentEntity {
+	
+	private int studentId;
+	private String studentName;
+	private String studentClass;
+	public int getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(int studentId) {
+		this.studentId = studentId;
+	}
+	public String getStudentName() {
+		return studentName;
+	}
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	public String getStudentClass() {
+		return studentClass;
+	}
+	public void setStudentClass(String studentClass) {
+		this.studentClass = studentClass;
+	}
+	
+	
+
+}
---
+++ test/src/main/java/com/test/TestCollection.java
@@ -0,0 +1,41 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
+public class TestCollection {
+
+	public static void main(String[] args) {
+		
+//		List collect = new ArrayList();
+//		collect.add(55);
+//		collect.add(1);
+//		collect.add(18);
+//		
+//		collect.add(0, 17);
+//		
+//		Collections.sort(collect); //Sortting Technique 
+//		
+//		collect.forEach(System.out::println);//lamda Expression it's a printing statement,Stream API
+		
+//		Vector v = new Vector();
+//		v.add(11);
+//		v.add(12);
+//		v.add(13);
+//		v.add(14);
+//		v.add(15);
+//		v.add(16);
+//		v.add(17);
+//		v.add(18);
+//		v.add(19);
+//		v.add(20);
+//		//v.add(15);
+//		v.forEach(System.out::println);
+//		System.out.println(v.capacity());
+		
+		
+	}
+
+}
